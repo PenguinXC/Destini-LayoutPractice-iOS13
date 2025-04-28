f9b0a183c52a91d542756263f04cf921c7da8386
PengiunXC <113483621+PenguinXC@users.noreply.github.com>
2025-04-28 23:16:32 +0700

    115. Step 6: Make the StoryBrain more Scalable

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -12,11 +12,15 @@ struct Story {
     let title: String
     let choice0: String
     let choice1: String
+    let choice0Destination: Int
+    let choice1Destination: Int
     
-    init(title: String, choice0: String, choice1: String) {
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
         self.title = title
-        self.choice0 = choice0
-        self.choice1 = choice1
+        self.choice0 = choice1
+        self.choice0Destination = choice1Destination
+        self.choice1 = choice2
+        self.choice1Destination = choice2Destination
     }
 }
 
